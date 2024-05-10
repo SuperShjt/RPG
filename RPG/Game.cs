6bd24e54f4019764ce68ff7a91bccef09b5280e3
Marcelino <marconaoum@gmail.com>
2024-05-10 21:29:07 +0300

    RPG

--- RPG/Game.cs
+++ RPG/Game.cs
@@ -1,17 +1,27 @@
+using Engine;
+
+
+
 namespace RPG
 {
     public partial class Game : Form
     {
+        private Player P1;
         public Game()
         {
             InitializeComponent();
+            P1 = new Player();
+            P1.MaximumHitPoints = 100;
+            P1.CurrentHitPoints = 100;
+            P1.Gold = 1000;
+            P1.ExperiencePoints = 0;
+            P1.Level = 1;
+            lblHit.Text= P1.CurrentHitPoints.ToString();
+            lblGold.Text= P1.Gold.ToString();
+            lblLevel.Text= P1.Level.ToString();
+            lblExp.Text= P1.ExperiencePoints.ToString();
         }
 
-        private void BtnStart_Click(object sender, EventArgs e)
-        {
-            lblGold.Text = "1000 Gold";
-            lblLevel.Text = "1";
-            
-        }
+        
     }
 }
